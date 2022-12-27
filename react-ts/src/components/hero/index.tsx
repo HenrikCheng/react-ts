266762c39ea30dc03b0529899327694b6823bdd8
HenrikCheng <48348033+HenrikCheng@users.noreply.github.com>
2022-12-27 22:41:10 +0200

    added scroll blur & navbar

--- react-ts/src/components/hero/index.tsx
+++ react-ts/src/components/hero/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, MutableRefObject, useEffect } from "react";
+import Navbar from "../Navbar";
 
 interface AppProps {
   message: string;
@@ -9,7 +10,9 @@ const Hero = ({ message }: AppProps): JSX.Element => {
   const [mouseY, setMouseY] = useState();
   const [mouseXColor, setMouseXColor] = useState("");
   const [mouseYColor, setMouseYColor] = useState("");
-  const [screenY, setScreenY] = useState(0);
+  const [scrollTop, setScrollTop] = useState(0);
+  const [scrollColor, setScrollColor] = useState("");
+  const [scrollText, setScrollText] = useState(0);
 
   const heroBottomAnchor = useRef() as MutableRefObject<HTMLDivElement>;
 
@@ -72,43 +75,77 @@ const Hero = ({ message }: AppProps): JSX.Element => {
       setMouseXColor("from-indigo-50");
     } else setMouseXColor("from-white");
   }
+  const onScroll = (e: any) => {
+    setScrollTop(e.target.documentElement.scrollTop);
+  };
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+  }, []);
+
+  useEffect(() => {
+    const onScroll = (e: any) => {
+      setScrollTop(e.target.documentElement.scrollTop);
+    };
+    window.addEventListener("scroll", onScroll);
+
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop]);
+
+  function updateHero(scrollTop: number) {
+    if (-130 > scrollText) return setScrollColor("blur-xl");
+    else if (-100 > scrollText) return setScrollColor("blur-lg");
+    else if (-70 > scrollText) return setScrollColor("blur-md");
+    else if (-40 > scrollText) return setScrollColor("blur");
+    else if (0 > scrollText) return setScrollColor("blur-sm");
+    else return setScrollColor("");
+  }
+
+  useEffect(() => {
+    updateHero(scrollTop);
+  }, [scrollTop]);
 
   return (
-    <div
-      className={`${mouseXColor} bg-gradient-to-r ${mouseYColor} h-screen flex justify-center items-center flex-col relative saturate-200 group`}
-      ref={(el) => {
-        if (!el) return;
-        console.log(el.getBoundingClientRect().top);
-      }}
-    >
+    <div className="h-screen flex flex-col">
+      <Navbar />
       <div
-        id="TextContainer"
-        className={`absolute ${
-          mouseY && mouseY > 300
-            ? "text-gray-100"
-            : "text-gray-900 shadow-indigo-500/40"
-        }`}
+        className={`${mouseXColor} bg-gradient-to-r ${mouseYColor} grow flex justify-center items-center flex-col group saturate-200 ${scrollColor}`}
       >
-        <h1 className={`text-7xl bold ${transitionClasses}`}>
-          Merry Christmas
-        </h1>
-        <h3 className={`text-4xl bold mb-5 ${transitionClasses}`}>
-          & Happy New Year
-        </h3>
-        <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-xl mb-3"
-          onClick={executeScroll}
+        <div
+          id="TextContainer"
+          className={`${
+            (mouseY && mouseY > 600) ||
+            (mouseX && mouseX > 400 && mouseY && mouseY > 400)
+              ? "text-gray-100"
+              : "text-gray-900"
+          }`}
         >
-          Continue
-        </button>
-        <div className="flex flex-row justify-center space-x-6">
-          <div className="text-semibold text-lg">X: {mouseX}</div>
-          <div className="text-semibold text-lg">Y: {mouseY}</div>
+          <h1
+            ref={(el) => {
+              if (!el) return;
+              setScrollText(el.getBoundingClientRect().top);
+            }}
+            className={`text-7xl bold ${transitionClasses}`}
+          >
+            Merry Christmas
+          </h1>
+          <h3 className={`text-4xl bold mb-5 ${transitionClasses}`}>
+            & Happy New Year
+          </h3>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-xl mb-3"
+            onClick={executeScroll}
+          >
+            Continue
+          </button>
+          <div className="flex flex-row justify-center space-x-6">
+            <div className="text-semibold text-lg">X: {mouseX}</div>
+            <div className="text-semibold text-lg">Y: {mouseY}</div>
+            <div className="text-semibold text-lg">scrollTop: {scrollTop}</div>
+          </div>
         </div>
+        <div ref={heroBottomAnchor} className="absolute bottom-0" />
       </div>
-      <div ref={heroBottomAnchor} className="absolute bottom-0" />
     </div>
   );
 };
