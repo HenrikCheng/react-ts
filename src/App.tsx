f2b74763d3fa0fc991def4137f1bdad812963c92
HenrikCheng <henrikcheng@live.se>
2023-03-30 12:04:02 +0200

    removed unused repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
+import React from "react";
+import Home from "./Pages/Home";
 import "./App.css";
-import { useDispatch, useSelector } from "react-redux";
-import { bindActionCreators } from "@reduxjs/toolkit";
-import { actionsCreators, State } from "./state";
 
 function App() {
-  const dispatch = useDispatch();
-
-  const { depositMoney, withdrawMoney, bankrupt } = bindActionCreators(
-    actionsCreators,
-    dispatch
-  );
-
-  const amount = useSelector((state: State) => state.bank);
-
   return (
     <div className="App">
-      <h1>{amount}</h1>
-      <button onClick={() => depositMoney(1000)}>Deposit</button>
-      <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
-      <button onClick={() => bankrupt()}>Bankrupt</button>
+      <Home />
     </div>
   );
 }
--- src/state/index.ts
+++
@@ -1,5 +0,0 @@
-export * as actionsCreators from "./action-creators";
-export * from "./store";
-export * from "./reducers";
-export * from "./store";
-export * as actionCreators from "./action-creators";
