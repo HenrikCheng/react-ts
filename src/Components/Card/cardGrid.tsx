63c150535df499d1eed2ded29619c181324ecec7
HenrikCheng <henrikcheng@live.se>
2023-07-01 22:23:46 +0200

    add test for horizontal side scroller

--- src/Components/Card/cardGrid.tsx
+++ src/Components/Card/cardGrid.tsx
@@ -1,11 +1,43 @@
 import React, { ReactNode } from "react";
 
+const Container = () => {
+  const itemArray = Array.from({ length: 10 });
+
+  return (
+    <div className="App" style={{ width: "100%", overflowX: "scroll" }}>
+      <div style={{ display: "flex" }}>
+        {itemArray.map((_, index) => (
+          <Item key={index} number={index} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+type ItemProps = {
+  number: number;
+};
+
+const Item = ({ number }: ItemProps) => {
+  return (
+    <div
+      style={{ height: "300px", width: "514px", margin: "16px" }}
+      className="bg-slate-500"
+    >
+      <div style={{ height: "100%", width: "514px" }}>Hello {number}</div>
+    </div>
+  );
+};
+
 interface CardContainerProps {
   children: ReactNode;
 }
 
 const CardGrid = ({ children }: CardContainerProps) => (
-  <div className="flex flex-col w-full items-center">{children}</div>
+  <div className="flex flex-col w-full items-center">
+    <Container />
+    {children}
+  </div>
 );
 
 export default CardGrid;
