4f445234875761ed6baa7f13e0d6d3516a3f1c3d
HenrikCheng <henrikcheng@live.se>
2023-08-04 16:39:06 +0200

    footer padding

--- src/Pages/Footer/index.tsx
+++ src/Pages/Footer/index.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer: React.FC<{}> = (props) => {
   const currentYear = new Date().getFullYear();
   return (
-    <div className="w-full fixed bottom-0 -z-10 bg-slate-200 pt-20">
+    <div className="w-full fixed bottom-0 -z-10 bg-slate-200 pt-20 pb-4">
       &copy; {currentYear} Henrik Cheng. All rights reserved.
     </div>
   );
