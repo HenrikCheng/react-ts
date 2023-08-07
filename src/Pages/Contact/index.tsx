3ddfce0a3f344e7d0e5d349a8d7c5cc0fef9d8bc
HenrikCheng <henrikcheng@live.se>
2023-08-07 13:55:43 +0200

    contact bug fix

--- src/Pages/Contact/index.tsx
+++ src/Pages/Contact/index.tsx
@@ -3,35 +3,33 @@ import Header from "../../components/Header";
 
 const Contact: React.FC<{}> = (props) => {
   return (
-    <Card classes="flex flex-row container bg-white shadow rounded-xl lg:px-4 2xl:px-24 py-4">
-      <div>
-        <Header title="Kontakt" classes="text-4xl font-semibold mb-4" />
-        <p className="text-xl font-semibold mb-4">
-          För att komma i kontakt med Henrik som konsult kan du ta kontakt med:
-        </p>
-        <p>
-          Namn: <span className="font-semibold">Johan Lundborg</span>
-        </p>
-        <p>
-          Företag: <span className="font-semibold">Tretton37</span>
-        </p>
-        <p className="font-semibold">
-          Telefon: <a href="tel:+46708908222">+46708908222</a>
-        </p>
-        <p>
-          E-post:
-          <span className="font-semibold">
-            <a href="mailto:johan.lundborg@tretton37.com">
-              johan.lundborg@tretton37.com
-            </a>
-          </span>
-        </p>
-        <p className="mt-4">
-          Tveka inte att höra av dig om du har några frågor eller om du är
-          intresserad av att samarbeta med Henrik. Johan ser fram emot att höra
-          från dig och hjälper dig gärna vidare i processen!
-        </p>
-      </div>
+    <Card classes="container bg-white shadow rounded-xl lg:px-4 2xl:px-24 py-4">
+      <Header title="Kontakt" classes="text-4xl font-semibold mb-4" />
+      <p className="text-xl font-semibold mb-4" id="Contact">
+        För att komma i kontakt med Henrik som konsult kan du ta kontakt med:
+      </p>
+      <p>
+        Namn: <span className="font-semibold">Johan Lundborg</span>
+      </p>
+      <p>
+        Företag: <span className="font-semibold">Tretton37</span>
+      </p>
+      <p className="font-semibold">
+        Telefon: <a href="tel:+46708908222">+46708908222</a>
+      </p>
+      <p>
+        E-post:
+        <span className="font-semibold">
+          <a href="mailto:johan.lundborg@tretton37.com">
+            johan.lundborg@tretton37.com
+          </a>
+        </span>
+      </p>
+      <p className="mt-4">
+        Tveka inte att höra av dig om du har några frågor eller om du är
+        intresserad av att samarbeta med Henrik. Johan ser fram emot att höra
+        från dig och hjälper dig gärna vidare i processen!
+      </p>
     </Card>
   );
 };
