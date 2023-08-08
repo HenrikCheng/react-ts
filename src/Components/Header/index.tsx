1145bca91466299c7cc0db53948121f6afa8e8ec
HenrikCheng <henrikcheng@live.se>
2023-08-08 09:10:44 +0200

    add children option to text & header

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,16 +1,25 @@
-import React from "react"; // we need this to make JSX compile
+import React, { ReactNode } from "react";
 
 interface HeaderProps {
-  title: string;
+  title?: string;
   subTitle?: string;
   classes?: string;
+  children?: ReactNode;
 }
 
-const Header = ({ title, subTitle, classes }: HeaderProps) => (
-  <div>
-    <div className={classes || "font-bold text-xl mb-2"}>{title}</div>
-    {subTitle && <p>{subTitle}</p>}
-  </div>
-);
+const Header = ({ title, subTitle, classes, children }: HeaderProps) => {
+  if (title && children) {
+    throw new Error("Only one of 'title' or 'children' should be provided.");
+  }
+
+  return (
+    <div>
+      <div className={classes || "font-bold text-xl mb-2"}>
+        {title || children}
+      </div>
+      {subTitle && <p>{subTitle}</p>}
+    </div>
+  );
+};
 
 export default Header;
--- src/Components/Text/index.tsx
+++ src/Components/Text/index.tsx
@@ -1,13 +1,16 @@
-import React from "react"; // we need this to make JSX compile
+import React, { ReactNode } from "react"; // we need this to make JSX compile
 
 interface HeaderProps {
-  content: string;
+  content?: string;
   classes?: string;
+  children?: ReactNode;
 }
 
-const Header = ({ content, classes }: HeaderProps) => (
+const Header = ({ content, classes, children }: HeaderProps) => (
   <div>
-    <p className={classes || "text-gray-700 text-base text-left mb-6 mx-2"}>{content}</p>
+    <p className={classes || "text-gray-700 text-base text-left mb-6 mx-2"}>
+      {content || children}
+    </p>
   </div>
 );
 
