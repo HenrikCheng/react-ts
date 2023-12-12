413dd5a403a12d878e0dcc46071abcd88e5ce99f
HenrikCheng <henrikcheng@live.se>
2023-12-12 15:03:39 +0100

    improve carousel handling

--- src/Components/Slider/Slide.tsx
+++ src/Components/Slider/Slide.tsx
@@ -1,100 +1,99 @@
 import { FormattedMessage } from "react-intl";
 
 type ItemProps = {
-  href?: string;
-  imageSrc: string;
-  header?: string;
-  description?: string;
-  techStack?: Array<String>;
-  index: number;
-  isEnd?: boolean;
-  length: number;
-  height?: number;
-  width?: number;
-  backgroundColor?: string;
-  githubLink?: string;
+	href?: string;
+	imageSrc: string;
+	header?: string;
+	description?: string;
+	techStack?: Array<String>;
+	index: number;
+	isEnd?: boolean;
+	length: number;
+	height?: number;
+	width?: number;
+	backgroundColor?: string;
+	githubLink?: string;
 };
 
 const Slide = ({
-  href,
-  imageSrc,
-  header,
-  description,
-  techStack,
-  index,
-  isEnd,
-  length,
-  height,
-  width,
-  backgroundColor,
-  githubLink,
+	href,
+	imageSrc,
+	header,
+	description,
+	techStack,
+	index,
+	isEnd,
+	length,
+	height,
+	width,
+	backgroundColor,
+	githubLink,
 }: ItemProps) => {
-  return (
-    <div className="ml-6 last:pr-6 relative">
-      {(href || githubLink) && (
-        <div className="w-full flex justify-around mb-1">
-          {href && (
-            <a
-              className="font-bold hover:underline underline-offset-2"
-              href={href}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FormattedMessage id="demo_open" />
-            </a>
-          )}
-          {githubLink && (
-            <a
-              className="font-bold hover:underline underline-offset-2"
-              href={githubLink}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <FormattedMessage id="demo_sourcecode" />
-            </a>
-          )}
-        </div>
-      )}
+	return (
+		<div className="ml-6 last:pr-6 relative">
+			<div
+				className="relative overflow-y-scrol drop-shadow-2xl"
+				style={{ height: `${height}px`, width: `${width}px` }}
+			>
+				{(header || description) && (
+					<div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-white opacity-90 text-transparent focus:text-black active:hover:text-black hover:text-black p-6 text-lg font-medium flex-col overflow-y-auto group">
+						{header && (
+							<h5 className="font-bold">
+								<FormattedMessage id={header} />
+							</h5>
+						)}
+						{description && (
+							<p className="text-justify">
+								<FormattedMessage id={description} />
+							</p>
+						)}
+						{(href || githubLink) && (
+							<div className="w-full group-hover:flex justify-around mt-2 hidden">
+								{href && (
+									<a
+										className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-xl"
+										href={href}
+										target="_blank"
+										rel="noreferrer"
+									>
+										<FormattedMessage id="demo_open" />
+									</a>
+								)}
+								{githubLink && (
+									<a
+										className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-3 px-6 rounded-xl"
+										href={githubLink}
+										target="_blank"
+										rel="noreferrer"
+									>
+										<FormattedMessage id="demo_sourcecode" />
+									</a>
+								)}
+							</div>
+						)}
+					</div>
+				)}
+				<img
+					src={imageSrc}
+					alt={`representation of ${imageSrc}`}
+					className={`hover:cursor-pointer object-contain rounded-lg ${backgroundColor}`}
+					style={{ height, width }}
+				/>
+			</div>
 
-      <div
-        className="relative overflow-y-scrol drop-shadow-2xl"
-        style={{ height: `${height}px`, width: `${width}px` }}
-      >
-        {(header || description) && (
-          <div className="absolute top-0 bottom-0 left-0 right-0 hover:bg-white opacity-90 text-transparent focus:text-black active:hover:text-black hover:text-black p-6 text-lg font-medium flex-col overflow-y-auto">
-            {header && (
-              <h5 className="font-bold">
-                <FormattedMessage id={header} />
-              </h5>
-            )}
-            {description && (
-              <p className="text-justify">
-                <FormattedMessage id={description} />
-              </p>
-            )}
-          </div>
-        )}
-        <img
-          src={imageSrc}
-          alt={`representation of ${imageSrc}`}
-          className={`hover:cursor-pointer object-contain rounded-lg ${backgroundColor}`}
-          style={{ height, width }}
-        />
-      </div>
+			{techStack && (
+				<div className="font-bold font-mono tracking-widest mt-2">
+					{techStack.join(" | ")}
+				</div>
+			)}
 
-      {techStack && (
-        <div className="font-bold font-mono tracking-widest mt-2">
-          {techStack.join(" | ")}
-        </div>
-      )}
-
-      {isEnd && index === length - 1 && (
-        <div
-          className={`bg-gradient-to-l from-slate-400 absolute top-0 bottom-0 right-0 w-36 opacity-50`}
-        />
-      )}
-    </div>
-  );
+			{isEnd && index === length - 1 && (
+				<div
+					className={`bg-gradient-to-l from-slate-400 absolute top-0 bottom-0 right-0 w-36 opacity-50`}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default Slide;
--- src/Pages/Demo/index.tsx
+++ src/Pages/Demo/index.tsx
@@ -15,95 +15,95 @@ import Mirjaleipoo from "../../images/mirjaleipoo.png";
 import People from "../../images/1337-people.png";
 
 const Demo: React.FC<{}> = (props) => {
-  const projects = [
-    {
-      href: "https://henrikcheng.github.io/react-ts",
-      imageSrc: PortfolioPic,
-      imageAlt: "Henriks portfolio page",
-      header: "demo_portfolio_title",
-      description: "demo_portfolio_description",
-      techStack: ["Typescript", "React", "React-Intl", "OpenLayers maps"],
-    },
-    {
-      href: "https://www.tretton37.com/",
-      imageSrc: tretton37,
-      imageAlt: "tretton37 website",
-      header: "demo_portfolio_title",
-      description: "demo_tretton_description",
-      techStack: ["Webflow", "HTML", "JS", "CSS", "Kanban", "Figma"],
-    },
-    {
-      href: "https://henrikcheng.github.io/1337-people",
-      imageSrc: People,
-      imageAlt: "Company employees of tretton37",
-      githubLink: "https://github.com/HenrikCheng/1337-people",
-      header: "demo_portfolio_title",
-      description: "demo_leetpeople_description",
-      techStack: ["React", "Query params", "Debounce", "Tailwind", "REST API"],
-    },
-    {
-      href: "https://henrikcheng.github.io/",
-      imageSrc: Mirjaleipoo,
-      imageAlt: "Website for a local bakery in Finland",
-      githubLink: "https://github.com/HenrikCheng/MirjaPage",
-      header: "demo_portfolio_title",
-      description: "demo_mirjaleipoo_description",
-      techStack: ["JS", "HTML", "CSS", "jQuery", "Bootstrap"],
-    },
-    {
-      href: "https://henrikcheng.github.io/film-app/",
-      imageSrc: FilmPic,
-      imageAlt: "click to go to video app",
-      githubLink: "https://github.com/HenrikCheng/film-app",
-      header: "demo_portfolio_title",
-      description: "demo_filmapp_description",
-      techStack: ["React", "JS", "HTML", "CSS", "Local storage", "Bootstrap"],
-    },
-    {
-      href: "https://henrikcheng.github.io/tic-tac-toe/",
-      imageSrc: TicTacToePic,
-      imageAlt: "click to go to tictactoe game",
-      githubLink: "https://github.com/HenrikCheng/tic-tac-toe",
-      header: "demo_portfolio_title",
-      description: "demo_tictactoe_description",
-      techStack: ["Typescript", "HTML", "CSS", "Tailwind", "React"],
-    },
-    {
-      href: "https://henrikcheng.github.io/weather-guru",
-      imageSrc: "https://picsum.photos/1280/768",
-      imageAlt: "Find out the weather at your location",
-      githubLink: "https://github.com/HenrikCheng/weather-guru",
-      header: "demo_weatherguru_title",
-      techStack: ["React", "REST-API"],
-      description: "demo_weatherguru_description",
-    },
-  ];
+	const projects = [
+		{
+			href: "https://henrikcheng.github.io/react-ts",
+			imageSrc: PortfolioPic,
+			imageAlt: "Henriks portfolio page",
+			header: "demo_portfolio_title",
+			description: "demo_portfolio_description",
+			techStack: ["Typescript", "React", "React-Intl", "OpenLayers maps"],
+		},
+		{
+			href: "https://www.tretton37.com/",
+			imageSrc: tretton37,
+			imageAlt: "tretton37 website",
+			header: "demo_portfolio_title",
+			description: "demo_tretton_description",
+			techStack: ["Webflow", "HTML", "JS", "CSS", "Kanban", "Figma"],
+		},
+		{
+			href: "https://henrikcheng.github.io/1337-people",
+			imageSrc: People,
+			imageAlt: "Company employees of tretton37",
+			githubLink: "https://github.com/HenrikCheng/1337-people",
+			header: "demo_portfolio_title",
+			description: "demo_leetpeople_description",
+			techStack: ["React", "Query params", "Debounce", "Tailwind", "REST API"],
+		},
+		{
+			href: "https://henrikcheng.github.io/",
+			imageSrc: Mirjaleipoo,
+			imageAlt: "Website for a local bakery in Finland",
+			githubLink: "https://github.com/HenrikCheng/MirjaPage",
+			header: "demo_portfolio_title",
+			description: "demo_mirjaleipoo_description",
+			techStack: ["JS", "HTML", "CSS", "jQuery", "Bootstrap"],
+		},
+		{
+			href: "https://henrikcheng.github.io/film-app/",
+			imageSrc: FilmPic,
+			imageAlt: "click to go to video app",
+			githubLink: "https://github.com/HenrikCheng/film-app",
+			header: "demo_portfolio_title",
+			description: "demo_filmapp_description",
+			techStack: ["React", "JS", "HTML", "CSS", "Local storage", "Bootstrap"],
+		},
+		{
+			href: "https://henrikcheng.github.io/tic-tac-toe/",
+			imageSrc: TicTacToePic,
+			imageAlt: "click to go to tictactoe game",
+			githubLink: "https://github.com/HenrikCheng/tic-tac-toe",
+			header: "demo_portfolio_title",
+			description: "demo_tictactoe_description",
+			techStack: ["Typescript", "HTML", "CSS", "Tailwind", "React"],
+		},
+		{
+			href: "https://henrikcheng.github.io/weather-guru",
+			imageSrc: "https://picsum.photos/1280/768",
+			imageAlt: "Find out the weather at your location",
+			githubLink: "https://github.com/HenrikCheng/weather-guru",
+			header: "demo_weatherguru_title",
+			techStack: ["React", "REST-API"],
+			description: "demo_weatherguru_description",
+		},
+	];
 
-  return (
-    <Card>
-      <div id="Demo">
-        <Header classes="text-4xl font-semibold mb-4">
-          <FormattedMessage id="demo_title" />
-        </Header>
-        <Text classes="text-gray-700 text-base text-left mb-6 mx-2 text-center">
-          <FormattedMessage id="demo_subtitle" />
-        </Text>
+	return (
+		<Card>
+			<div id="Demo">
+				<Header classes="text-4xl font-semibold mb-4">
+					<FormattedMessage id="demo_title" />
+				</Header>
+				<Text classes="text-gray-700 text-base text-left mb-6 mx-2 text-center">
+					<FormattedMessage id="demo_subtitle" />
+				</Text>
 
-        <Slider height={350} width={400} slides={projects} componentId="Demo" />
-        <div className="mt-6 mb-3">
-          <a
-            href="https://www.linkedin.com/in/henrik-cheng/"
-            target="_blank"
-            rel="noreferrer"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-xl "
-          >
-            <FormattedMessage id="cv_more_info" />
-            <FontAwesomeIcon icon={faArrowUpRightFromSquare} className="ml-2" />
-          </a>
-        </div>
-      </div>
-    </Card>
-  );
+				<Slider height={350} width={400} slides={projects} componentId="Demo" />
+				<div className="mt-6 mb-3">
+					<a
+						href="https://www.linkedin.com/in/henrik-cheng/"
+						target="_blank"
+						rel="noreferrer"
+						className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-3 px-6 rounded-xl"
+					>
+						<FormattedMessage id="cv_more_info" />
+						<FontAwesomeIcon icon={faArrowUpRightFromSquare} className="ml-2" />
+					</a>
+				</div>
+			</div>
+		</Card>
+	);
 };
 
 export default Demo;
